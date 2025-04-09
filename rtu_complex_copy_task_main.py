81c3d003c0d6a08bd82a25715e6a139f6db37332
arber <arber@ualberta.ca>
2025-04-09 15:41:09 -0600

    commented out evaluation part

--- rtu_complex_copy_task_main.py
+++ rtu_complex_copy_task_main.py
@@ -353,47 +353,47 @@ for ep in range(num_epoch):
                f"End epoch {ep+1} =============")
         loginf(f"train loss: {acc_loss / steps}")
         # *************************************************************************************************************
-        eval_model.load_state_dict(model.state_dict())
-        v_loss, v_acc, v_acc_noop, v_acc_print = compute_accuracy(
-            eval_model, valid_data_loader, loss_fn, no_print_idx=no_print_idx,
-            pad_value=tgt_pad_idx, show_example=False)
-        loginf(f"valid loss: {v_loss}")
-        loginf(f"valid acc: {v_acc:.2f} %")
-        loginf(f"valid no-op acc: {v_acc_noop:.2f} %")
-        loginf(f"valid print acc: {v_acc_print:.2f} %")
+#         eval_model.load_state_dict(model.state_dict())
+#         v_loss, v_acc, v_acc_noop, v_acc_print = compute_accuracy(
+#             eval_model, valid_data_loader, loss_fn, no_print_idx=no_print_idx,
+#             pad_value=tgt_pad_idx, show_example=False)
+#         loginf(f"valid loss: {v_loss}")
+#         loginf(f"valid acc: {v_acc:.2f} %")
+#         loginf(f"valid no-op acc: {v_acc_noop:.2f} %")
+#         loginf(f"valid print acc: {v_acc_print:.2f} %")
 
-        if use_wandb:
-            wandb.log({"train_loss": acc_loss / steps})
-            wandb.log({"valid_loss": v_loss})
-            wandb.log({"valid_acc": v_acc})
-            wandb.log({"valid_acc_noop": v_acc_noop})
-            wandb.log({"valid_acc_print": v_acc_print})
+#         if use_wandb:
+#             wandb.log({"train_loss": acc_loss / steps})
+#             wandb.log({"valid_loss": v_loss})
+#             wandb.log({"valid_acc": v_acc})
+#             wandb.log({"valid_acc_noop": v_acc_noop})
+#             wandb.log({"valid_acc_print": v_acc_print})
 
-        if v_acc > best_val_acc:
-            best_val_acc = v_acc 
-            best_epoch = ep + 1
-            # Save the best model
-            loginf("The best model so far.")
-            torch.save({'epoch': best_epoch,
-                        'model_state_dict': model.state_dict(),
-                        'optimizer_state_dict': optimizer.state_dict(),
-                        'valid_acc': best_val_acc}, best_model_path)
-            loginf("Saved.")
-        # Save the latest model
-        torch.save({'epoch': ep + 1,
-                    'model_state_dict': model.state_dict(),
-                    'optimizer_state_dict': optimizer.state_dict(),
-                    'valid_acc': v_acc}, lastest_model_path)
+#         if v_acc > best_val_acc:
+#             best_val_acc = v_acc 
+#             best_epoch = ep + 1
+#             # Save the best model
+#             loginf("The best model so far.")
+#             torch.save({'epoch': best_epoch,
+#                         'model_state_dict': model.state_dict(),
+#                         'optimizer_state_dict': optimizer.state_dict(),
+#                         'valid_acc': best_val_acc}, best_model_path)
+#             loginf("Saved.")
+#         # Save the latest model
+#         torch.save({'epoch': ep + 1,
+#                     'model_state_dict': model.state_dict(),
+#                     'optimizer_state_dict': optimizer.state_dict(),
+#                     'valid_acc': v_acc}, lastest_model_path)
 
-        acc_loss = 0.0
-        steps = 0
+#         acc_loss = 0.0
+#         steps = 0
 
-    if v_acc >= stop_acc:
-        break
+#     if v_acc >= stop_acc:
+#         break
 
-elapsed = time.time() - start_time
-loginf(f"Ran {ep} epochs in {elapsed / 60.:.2f} min.")
-loginf(f"Best validation acc: {best_val_acc:.2f}")
+# elapsed = time.time() - start_time
+# loginf(f"Ran {ep} epochs in {elapsed / 60.:.2f} min.")
+# loginf(f"Best validation acc: {best_val_acc:.2f}")
 # *************************************************************************************************************
 
 # if best_epoch > 1:  # load the best model and evaluate on the test set
